c9b1236cc74cee150597c4d5635169afe1b3dadc
German Chiocchia <germanchiocchia@gmail.com>
2021-03-09 23:19:44 -0300

    1st migration and book table made

--- usingDjango3/demo/mysite/myapp/views.py
+++ usingDjango3/demo/mysite/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    return HttpResponse('<h1>Hello world</h1>')
+
+def products(request):
+    return HttpResponse('Products')
 # Create your views here.
