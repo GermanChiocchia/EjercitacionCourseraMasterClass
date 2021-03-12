9dfea00eb5e6a1f808a153359366f8a9a77d1cf7
German Chiocchia <germanchiocchia@gmail.com>
2021-03-12 18:15:43 -0300

    add, edit and delete functionalities done

--- usingDjango3/demo/mysite/myapp/urls.py
+++ usingDjango3/demo/mysite/myapp/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from myapp import views
 
@@ -6,6 +5,8 @@ app_name = 'myapp'
 
 urlpatterns = [
     path('', views.index, name= 'index'),
-    path('book/<int:book_id>', views.detail, name='detail'),
+    path('book/<int:book_id>/', views.detail, name='detail'),
     path('add/', views.add_book, name='add_book'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
--- usingDjango3/demo/mysite/myapp/views.py
+++ usingDjango3/demo/mysite/myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, request
 from .models import Book
+from .forms import BookForm
 
 def index(request):
     book_list = Book.objects.all()
@@ -22,6 +23,21 @@ def add_book(request):
 
         book = Book(name=name, desc=desc, price=price, book_image=book_image)
         book.save()
-        
+        return redirect('/')
     return render(request, 'myapp/add_book.html')
+
+def update(request, id):
+    book = Book.objects.get(id=id)
+    form = BookForm(request.POST or None, request.FILES, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'myapp/edit.html', {'form':form, 'book':book})
+
+def delete(request, id):
+    if request.method=='POST':
+        book = Book.objects.get(id=id)
+        book.delete()
+        return redirect('/')
+    return render(request, 'myapp/delete.html')
 # Create your views here.
