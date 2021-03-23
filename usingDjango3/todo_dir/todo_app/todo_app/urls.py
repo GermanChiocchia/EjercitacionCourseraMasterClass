0b58c2ecb81c4d7d0b730b8f737f6135c52c6e20
German Chiocchia <germanchiocchia@gmail.com>
2021-03-22 22:53:57 -0300

    add view, add template, add path, all done

--- usingDjango3/todo_dir/todo_app/todo_app/urls.py
+++ usingDjango3/todo_dir/todo_app/todo_app/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from one_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add/',views.add, name='add'),
 ]
