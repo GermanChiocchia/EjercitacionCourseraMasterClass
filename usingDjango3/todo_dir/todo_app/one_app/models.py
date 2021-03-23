73298522e8b28f72a93c7dc0530ef47dd582c9e7
German Chiocchia <germanchiocchia@gmail.com>
2021-03-23 13:41:17 -0300

    base.html done, bootstrap applyed, add ended

--- usingDjango3/todo_dir/todo_app/one_app/models.py
+++ usingDjango3/todo_dir/todo_app/one_app/models.py
@@ -4,5 +4,9 @@ from django.db.models.fields import CharField
 # Create your models here.
 
 class Task(models.Model):
+    
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=100)
     priority = models.IntegerField()
--- usingDjango3/todo_dir/todo_app/one_app/views.py
+++ usingDjango3/todo_dir/todo_app/one_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
 
 def add(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        priority = request.POST.get('priority', '')
+
+        task = Task(name=name, priority=priority)
+        task.save()
+
     return render(request, 'one_app/add.html')
